b9252e9fc3b086ba7cd227a310f5c8944d55ed48
Atik074 <atiksmats#@gmail.com>
2023-07-13 17:28:57 +0600

    delete confirm

---
+++ src/Pages/Home/PublicOrder/OrderRow.jsx
@@ -0,0 +1,42 @@
+
+
+const OrderRow = ({order ,handleDelete}) => {
+    const {_id , displayName ,serviceName,img , email, price ,} = order 
+
+ 
+    return (
+       <tr>
+        <th>
+          <label>
+          <button onClick={()=>handleDelete(_id)} className="btn btn-sm btn-circle btn-outline">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+          </button>
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center space-x-3">
+             <div className="avatar">
+              <div className="mask mask-squircle w-full h-12">
+                  <img className="w" src={img} alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div> 
+             
+            <div>
+              <div className="font-semibold text-xl ml-2">{displayName}</div>
+              <div className="opacity-50 text-lg">{email}</div>
+            </div>
+          </div>
+        </td>
+        <td className="text-[18px]">
+        {serviceName}
+        
+        </td>
+        <td className="text-[18px]">{price}</td>
+        <th>
+          <button className="btn btn-ghost ">Details</button>
+        </th>
+      </tr> 
+    );
+};
+
+export default OrderRow;
--- src/Pages/Home/PublicOrder/PublicOrder.jsx
+++ src/Pages/Home/PublicOrder/PublicOrder.jsx
@@ -1,9 +1,30 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
+import OrderRow from "./OrderRow";
 
 const PublicOrder = () => {
     const {user} = useContext(AuthContext)
-    const [orders ,setOrders] =useState([])
+    const [orders , setOrders] =useState([])
+
+    const handleDelete = id =>{
+      const proceed = confirm('Are you sure ?')
+      if(proceed){
+        fetch(`http://localhost:5000/orders/${id}` ,
+        {
+            method:'DELETE',
+            
+        })
+        .then(res =>res.json()) 
+        .then(data =>{
+            console.log(data)
+            if(data.deletedCount > 0){
+              alert('delete successfull')
+              const remaining = orders.filter(order => order._id !== id) 
+              setOrders(remaining)
+            }
+        })
+      }
+}
  
 
     const url = `http://localhost:5000/orders?email=${user?.email}`  
@@ -20,7 +41,36 @@ const PublicOrder = () => {
    
     return (
         <div>
-             data count are {orders.length}
+
+           <div className="overflow-x-auto">
+  <table className="table my-10">
+    {/* head */}
+    <thead>
+      <tr className="text-xl">
+        <th>Delete</th>
+        <th>Name</th>
+        <th>Service</th>
+        <th>Visit</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+       {
+        orders.map(order =><OrderRow
+        key={order._id}
+        order={order}
+        handleDelete={handleDelete}
+        
+        ></OrderRow>)
+       }
+     
+     
+    </tbody>
+
+    
+  </table>
+</div>
+
         </div>
     );
 };
